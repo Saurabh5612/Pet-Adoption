bb0cda40c87e791eb70baa739f8504cec4e58db7
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-12-03 10:44:30 -0500

    separating sign in/sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const { User, Animal } = require("../models");
 
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,44 +1,77 @@
-$(document).ready(function() {
+async function signupFormHandler(event) {
+  event.preventDefault();
 
-    var signUpForm = $("form#signup");
-    var usernameInput = $("input#signup_username");
-    var emailInput = $("input#signup_email");
-    var passwordInput = $("input#signup_password");
-  
-    signUpForm.on("submit", function(event) {
-      event.preventDefault();
-      var userData = {
-        username: usernameInput.val().trim(),
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
-  
-      // validate there's a username/email/password entered on submit //
-      if (!userData.username || !userData.email || !userData.password) {
-        return;
-      }
-      // If validated, run signUpUser //
-      signUpUser(userData.username, userData.email, userData.password);
-      usernameInput.val("");
-      emailInput.val("");
-      passwordInput.val("");
+  const username = document.querySelector("#signup-username").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { "Content-Type": "application/json" }
     });
-  
-    // Does a post to /api/signup - if successful redirect to the members dash
-    function signUpUser(username, email, password) {
-      $.post("/api/users", {
-        username: username,
-        email: email,
-        password: password
-      }).then(function(data) {
-        window.location.replace(data);
-      }).catch(handleLoginErr);
+
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
     }
+  }
+};
+
+document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
+
+
+
+
+
+// $(document).ready(function() {
+
+//     var signUpForm = $("form#signup");
+//     var usernameInput = $("input#signup_username");
+//     var emailInput = $("input#signup_email");
+//     var passwordInput = $("input#signup_password");
   
-    function handleLoginErr(err) {
-      $("#alert .msg").text(err.responseJSON);
-      $("#alert").fadeIn(500);
-    }
-});
+//     signUpForm.on("submit", function(event) {
+//       event.preventDefault();
+//       var userData = {
+//         username: usernameInput.val().trim(),
+//         email: emailInput.val().trim(),
+//         password: passwordInput.val().trim()
+//       };
+  
+//       // validate there's a username/email/password entered on submit //
+//       if (!userData.username || !userData.email || !userData.password) {
+//         return;
+//       }
+//       // If validated, run signUpUser //
+//       signUpUser(userData.username, userData.email, userData.password);
+//       usernameInput.val("");
+//       emailInput.val("");
+//       passwordInput.val("");
+//     });
+  
+//     // Does a post to /api/signup - if successful redirect to the members dash
+//     function signUpUser(username, email, password) {
+//       $.post("/api/users", {
+//         username: username,
+//         email: email,
+//         password: password
+//       }).then(function(data) {
+//         window.location.replace(data);
+//       }).catch(handleLoginErr);
+//     }
+  
+//     function handleLoginErr(err) {
+//       $("#alert .msg").text(err.responseJSON);
+//       $("#alert").fadeIn(500);
+//     }
+// });
 
   
